a5bc54f4a485998861af185e23638025cc6f8230
mardex1 <mardemcastro123@gmail.com>
2024-10-28 22:03:46 -0300

    Melhoras no código do NaiveBayesCategorico

--- GMNB.py
+++ GMNB.py
@@ -5,6 +5,40 @@ from read_arff import read_arff
 from multirotulo_to_monorotulo import make_monorotulo
 from sklearn.naive_bayes import GaussianNB, MultinomialNB, CategoricalNB
 
+def gera_ancestrais(classes):
+        ancestrais = {}
+        for c1 in classes:
+            for c2 in classes:
+                if c2 in c1 and c1 not in ancestrais:
+                    ancestrais[c1] = [c2]
+                elif c2 in c1 and c1 in ancestrais:
+                    ancestrais[c1].append(c2)
+        return ancestrais
+
+def f_measure_hierarquica(predictions, y_true, classes):
+    ancestrais = gera_ancestrais(classes)
+    f_measure = 0
+    numerador = 0
+    denominador_precision = 0
+    denominador_recall = 0
+    for classe_predita, classe_verdadeira in zip(predictions, y_true):
+        ancestrais_classe_predita = ancestrais[classe_predita]
+        ancestrais_classe_verdadeira = ancestrais[classe_verdadeira]
+        classes_comum = 0
+        for c1 in ancestrais_classe_predita:
+            for c2 in ancestrais_classe_verdadeira:
+                if c1 == c2:
+                    classes_comum += 1
+        numerador += classes_comum
+        denominador_precision += len(ancestrais_classe_predita)
+        denominador_recall += len(ancestrais_classe_verdadeira)
+    hierarchical_precision = numerador / denominador_precision
+    hierarchical_recall = numerador / denominador_recall
+    f_measure = (2 * hierarchical_precision * hierarchical_recall) / (hierarchical_precision + hierarchical_recall)
+    return f_measure
+        
+
+    pass
 class NaiveBayesH:
     def fit(self, X, y, hierarquia=None):
         n_samples, n_features = X.shape
@@ -12,9 +46,11 @@ class NaiveBayesH:
         self.classes = np.unique(y)
         n_classes = len(self.classes)
 
-        self.ancestrais = self.gera_dict_ancestrais(X, y) 
+        self.descendentes = self.gera_descendentes(X, y) 
+        self.ancestrais = gera_ancestrais(classes)
         
-
+        # Lista com um dicionário para cada feature.
+        # Nesse dicionário, teremos uma chave para cada classe, sendo os valores mais outros dicionários com os valores do atributo.
         self.feature_probs = [{} for _ in range(n_features)]
         self.prior_prob = np.zeros(n_classes, dtype=np.float64)
 
@@ -27,11 +63,12 @@ class NaiveBayesH:
             print(f'plano: {len(X_plano)}')
 
             n_instancias_classe_c = X_c.shape[0]
+            # Para cada feature, seleciona as contagens de cada valor nessa feature.
             for feature_idx in range(n_features):
                 feature_vals, counts = np.unique(X_c[:, feature_idx], return_counts=True)
+                # Armazena em um dicionário, pares valor do atributo e sua probabilidade (contagem do atributo / instâncias da classe c)
                 feature_prob = {val: count / n_instancias_classe_c for val, count in zip(feature_vals, counts)}
                 self.feature_probs[feature_idx][c] = feature_prob
-            # Cálculo a média, variância e probabilidade a priori (probabilidade de cada classe na base de dados)
             self.prior_prob[idx] = n_instancias_classe_c / float(n_samples)
 
     def predict(self, X_test, usefullness=False):
@@ -39,13 +76,18 @@ class NaiveBayesH:
             usefullness_list = self.calculate_usefullness()
         X_test = X_test.astype(np.float64)
         predictions = []
+        # Para cada instância que deseja predizer
         for x in X_test:
             posteriors = []
+            # Para cada classe
             for idx, c in enumerate(self.classes):
                 # log(P(c)) + log(P(x1|c)) + log(P(x2|c)) + log(P(x3|c)) + log(P(x4|c))
+                # Pega a probabilidade a priori da classe
                 prior = np.log(self.prior_prob[idx])
                 likelihood = 0
+                # para cada feature
                 for feature_idx, feature_val in enumerate(x):
+                    print(feature_idx, feature_val)
                     # Get the feature probability given the class
                     if feature_val in self.feature_probs[feature_idx][c]:
                         likelihood += np.log(self.feature_probs[feature_idx][c][feature_val])
@@ -61,22 +103,22 @@ class NaiveBayesH:
 
         return np.array(predictions)
 
-    def gera_dict_ancestrais(self, X, y):
-        ancestrais = {}
+    def gera_descendentes(self, X, y):
+        descendentes = {}
         for c1 in self.classes:
             for c2 in self.classes:
-                if c2 in c1 and c1 not in ancestrais:
-                    ancestrais[c1] = [c2]
-                elif c2 in c1 and c1 in ancestrais:
-                    ancestrais[c1].append(c2)
-        print(ancestrais)
-        return ancestrais
+                if c2 in c1 and c2 not in descendentes:
+                    descendentes[c2] = [c1]
+                elif c2 in c1 and c2 in descendentes:
+                    descendentes[c2].append(c1)
+        print(descendentes)
+        return descendentes
     
     def calculate_usefullness(self):
-        max_tree_size = max([len(value) for key, value in self.ancestrais.items()])
+        max_tree_size = max([len(value) for key, value in self.descendentes.items()])
         usefullness = []
         for idx, c in enumerate(self.classes):
-            tree_size_i = len(self.ancestrais[c])
+            tree_size_i = len(self.descendentes[c])
             usefullness_i = 1 - (np.log2(tree_size_i) / max_tree_size)
             usefullness.append(usefullness_i)
         return usefullness
@@ -114,21 +156,26 @@ if __name__ == "__main__":
     # [6.73, 15,"R.1.2"]])
     # X = data[:, :-1].astype(np.float64)
     # y = data[:, -1]
+    classes = np.unique(y)
+
 
     model = NaiveBayesH()
-    
     model.fit(X, y)
     predictions = model.predict(X, usefullness=True)
-    print(sum(predictions == y) / len(y))
+    print("Accuracy: ", sum(predictions == y) / len(y))
+    f1_hier = f_measure_hierarquica(predictions, y, classes)
+    print('F-measure-hierarquica: ', f1_hier)
 
     model_sk = CategoricalNB()
     model_sk.fit(X, y)
     predictions = model_sk.predict(X)
     print(model_sk.get_params())
-    print(sum(predictions == y) / len(y))
+    print("Accuracy: ", sum(predictions == y) / len(y))
+    f1_hier = f_measure_hierarquica(predictions, y, classes)
+    print('F-measure-hierarquica: ', f1_hier)
 
 
-    # Xtr, Xts, ytr, yts = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Xtr, Xts, y, yts = train_test_split(X, y, test_size=0.2, random_state=42)
     
     # print(X)
     # print(y)
